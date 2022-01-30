74abffc57841038a9d45dd8e919437f5f47c3a9c
shiyanshirani <shiyan99s@gmail.com>
2022-01-30 14:40:49 +0530

    env variables added

--- content_service/content_service/settings.py
+++ content_service/content_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@b1!j-w4zylk&1kx7^uu^6$qts8+_!eip+z(y9mf)-_an*t5rb"
+SECRET_KEY = os.environ["CONTENT_SERVICE_API"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
