1ed32dd2314017d3c02e045994a1ffe0846621a3
shiyanshirani <shiyan99s@gmail.com>
2022-02-13 16:43:27 +0530

    secret key added

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["USER_SERVICE_API"]
+# SECRET_KEY = os.environ["USER_SERVICE_API"]
+SECRET_KEY = "django-insecure-6n-vx0vu$!k271c$13nv=p!dsmjh6%746^g=awk=*7ys1nqrb& "
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
