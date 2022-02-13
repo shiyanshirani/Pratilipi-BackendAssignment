b2d7cca941b9a789fe39841fa30d0eae85f047de
shiyanshirani <shiyan99s@gmail.com>
2022-02-13 16:43:15 +0530

    secret key added

--- user_interaction_service/user_interaction_service/settings.py
+++ user_interaction_service/user_interaction_service/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["USER_INTERACTION_SERVICE_API"]
+# SECRET_KEY = os.environ["USER_INTERACTION_SERVICE_API"]
+SECRET_KEY = "django-insecure-l@q83m1!4(=f7s=_7#oub65y1xm^gbozy(qzru)a&!n6c276)%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
