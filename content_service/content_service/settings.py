cbc97ec1f355c0fcff86935b0753e22e8d54f671
shiyanshirani <shiyan99s@gmail.com>
2022-02-13 16:42:58 +0530

    secret key added

--- content_service/content_service/settings.py
+++ content_service/content_service/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["CONTENT_SERVICE_API"]
+# SECRET_KEY = os.environ["CONTENT_SERVICE_API"]
+SECRET_KEY = "django-insecure-@b1!j-w4zylk&1kx7^uu^6$qts8+_!eip+z(y9mf)-_an*t5rb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
